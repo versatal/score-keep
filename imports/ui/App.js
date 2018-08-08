3a8fc95a0d5937584fcf84379849ac2769bc1df8
Roger Hardin <volcivar@yahoo.com>
2018-08-08 04:28:47 -0400

    changed title to full name

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <TitleBar title={this.props.title} subtitle="Created by Joe Hardin"/>
+        <TitleBar title={this.props.title} subtitle="Created by Roger Hardin"/>
         <div className="wrapper">
           <PlayerList players={this.props.players}/>
           <AddPlayer/>
